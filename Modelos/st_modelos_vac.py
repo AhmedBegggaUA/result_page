aa1618d24d443476e6d9797ee8f9e73ae6c7c8ae
Ahmed Begga <abh28@alu.ua.es>
2023-05-13 00:38:55 +0200

    web dependencies

--- Modelos/st_modelos_vac.py
+++ Modelos/st_modelos_vac.py
@@ -94,6 +94,10 @@ try:
         lista_idx = [0,1,2,3]
         idx = months_list.index(month)
         h7_waning = pd.read_csv('Modelos/last_pred/h7_waning_pred.csv')
+        h7_casos = pd.read_csv('Modelos/last_pred/h7_casos_pred.csv')
+        none_waning = pd.read_csv('Modelos/last_pred/none_waning_pred.csv')
+        none_casos = pd.read_csv('Modelos/last_pred/none_casos_pred.csv')
+        xprize = pd.read_csv('Modelos/last_pred/xprize_pred.csv')
         # Drop duplicated rows
         
         h7_waning = h7_waning[h7_waning['CountryName'] == country2]
@@ -101,19 +105,50 @@ try:
         h7_waning['month'] = h7_waning['fecha'].dt.strftime('%Y-%m')
         st.write(" La selección es: ", months_list_short[idx])
         h7_waning = h7_waning[h7_waning['month'] == months_list_short[idx]]
-        #h7_waning = h7_waning.drop_duplicates(subset=['CountryName','fecha'], keep='last')
-        # Agrupamos por fecha y pais 
         h7_waning = h7_waning.groupby(['CountryName','fecha']).mean().reset_index()
-        st.write(h7_waning)
-        # Let's get the smoothed data
         h7_waning['pred'] = h7_waning['pred'].rolling(window=7, min_periods=1).mean()
         h7_waning['pred_sir'] = h7_waning['pred_sir'].rolling(window=7, min_periods=1).mean()
         h7_waning['truth'] = h7_waning['truth'].rolling(window=7, min_periods=1).mean()
-        st.write(h7_waning)
+        
+        h7_casos = h7_casos[h7_casos['CountryName'] == country2]
+        h7_casos['fecha'] = pd.to_datetime(h7_casos['fecha'])
+        h7_casos['month'] = h7_casos['fecha'].dt.strftime('%Y-%m')
+        h7_casos = h7_casos[h7_casos['month'] == months_list_short[idx]]
+        h7_casos = h7_casos.groupby(['CountryName','fecha']).mean().reset_index()
+        h7_casos['pred'] = h7_casos['pred'].rolling(window=7, min_periods=1).mean()
+        h7_casos['pred_sir'] = h7_casos['pred_sir'].rolling(window=7, min_periods=1).mean()
+        
+        none_waning = none_waning[none_waning['CountryName'] == country2]
+        none_waning['fecha'] = pd.to_datetime(none_waning['fecha'])
+        none_waning['month'] = none_waning['fecha'].dt.strftime('%Y-%m')
+        none_waning = none_waning[none_waning['month'] == months_list_short[idx]]
+        none_waning = none_waning.groupby(['CountryName','fecha']).mean().reset_index()
+        none_waning['pred'] = none_waning['pred'].rolling(window=7, min_periods=1).mean()
+        none_waning['pred_sir'] = none_waning['pred_sir'].rolling(window=7, min_periods=1).mean()
+        
+        none_casos = none_casos[none_casos['CountryName'] == country2]
+        none_casos['fecha'] = pd.to_datetime(none_casos['fecha'])
+        none_casos['month'] = none_casos['fecha'].dt.strftime('%Y-%m')
+        none_casos = none_casos[none_casos['month'] == months_list_short[idx]]
+        none_casos = none_casos.groupby(['CountryName','fecha']).mean().reset_index()
+        none_casos['pred'] = none_casos['pred'].rolling(window=7, min_periods=1).mean()
+        none_casos['pred_sir'] = none_casos['pred_sir'].rolling(window=7, min_periods=1).mean()
+        
+        xprize = xprize[xprize['CountryName'] == country2]
+        xprize['fecha'] = pd.to_datetime(xprize['fecha'])
+        xprize['month'] = xprize['fecha'].dt.strftime('%Y-%m')
+        xprize = xprize[xprize['month'] == months_list_short[idx]]
+        xprize = xprize.groupby(['CountryName','fecha']).mean().reset_index()
+        xprize['pred'] = xprize['pred'].rolling(window=7, min_periods=1).mean()
+        xprize['pred_sir'] = xprize['pred_sir'].rolling(window=7, min_periods=1).mean()
+        
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['pred'], name = "Predicted Daily New Cases SVIR"))
-        fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['pred_sir'], name = "Predicted Daily New Cases SIR"))
-        fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['truth'], name = "Ground Truth Daily New Cases"))
+        fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['pred'], name = "H7 & VacW SVIR", line = dict(color = 'red', width = 2)))
+        fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['pred_sir'], name = "H7 & VacW SIR", line = dict(color = 'green', width = 2)))
+        fig.add_trace(go.Scatter(x = none_waning['fecha'], y = none_waning['pred'], name = "No H7 & VacW SVIR", line = dict(color = 'blue', width = 2)))
+        fig.add_trace(go.Scatter(x = none_waning['fecha'], y = none_waning['pred_sir'], name = "No H7 & VacW SIR", line = dict(color = 'purple', width = 2)))
+        fig.add_trace(go.Scatter(x = xprize['fecha'], y = xprize['pred'], name = "XPrize", line = dict(color = 'black', width = 2)))
+        fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['truth'], name = "Ground Truth Daily New Cases", line = dict(color = 'orange', width = 4, dash = 'dash')))
         #new_data = new_data.set_index("Date")
         fig.update_layout(
       margin=dict(l=20, r=20, t=20, b=20))
@@ -141,13 +176,13 @@ try:
         fig.update_xaxes(visible=True, showgrid=True, gridwidth=1, gridcolor='white',  tickformat="%b %d\n")
         fig.update_yaxes(visible=True, showgrid=True, gridwidth=1, gridcolor='white' )        
         # Let's show the legend in the top right corner
-        #fig.update_layout(legend=dict(
-        #yanchor="top",
-        #y=0.99,
-        #xanchor="left",
-        #x=0.01,
-        #font_size = 15
-      #))
+        fig.update_layout(legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        font_size = 15
+      ))
         fig.update_xaxes(title_font_size=30, tickfont_size=24)
         fig.update_yaxes(title_font_size=30, tickfont_size=24)
 
