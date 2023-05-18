a6a24f1551d40eb9ec903330f7a6b3b8a175d1da
Ahmed Begga <abh28@alu.ua.es>
2023-05-18 17:01:08 +0200

    Some bugs reported

--- Modelos/st_modelos_vac.py
+++ Modelos/st_modelos_vac.py
@@ -94,7 +94,6 @@ try:
         lista_idx = [0,1,2,3]
         idx = months_list.index(month)
         h7_waning = pd.read_csv('Modelos/last_pred/h7_waning_pred.csv')
-        st.write(h7_waning)
         h7_casos = pd.read_csv('Modelos/last_pred/h7_casos_pred.csv')
         none_waning = pd.read_csv('Modelos/last_pred/none_waning_pred.csv')
         none_casos = pd.read_csv('Modelos/last_pred/none_casos_pred.csv')
@@ -108,18 +107,16 @@ try:
             h7_waning = h7_waning[h7_waning['CountryName'] == country2]
             h7_waning['fecha'] = pd.to_datetime(h7_waning['fecha'])
             h7_waning['month'] = h7_waning['fecha'].dt.strftime('%Y-%m')
-            fecha_elegida = pd.to_datetime(months_list_short[idx]).strftime('%Y-%m')
-            st.write(fecha_elegida)
-            h7_waning = h7_waning[h7_waning['month'] == fecha_elegida]
+            h7_waning = h7_waning[h7_waning['month'] == months_list_short[idx]]
             h7_waning = h7_waning.groupby(['CountryName','fecha']).mean().reset_index()
             h7_waning['pred'] = h7_waning['pred'].rolling(window=7, min_periods=1).mean()
             h7_waning['pred_sir'] = h7_waning['pred_sir'].rolling(window=7, min_periods=1).mean()
             h7_waning['truth'] = h7_waning['truth'].rolling(window=7, min_periods=1).mean()
-            st.write(h7_waning)
+            
             h7_casos = h7_casos[h7_casos['CountryName'] == country2]
             h7_casos['fecha'] = pd.to_datetime(h7_casos['fecha'])
             h7_casos['month'] = h7_casos['fecha'].dt.strftime('%Y-%m')
-            h7_casos = h7_casos[h7_casos['month'] == fecha_elegida]
+            h7_casos = h7_casos[h7_casos['month'] == months_list_short[idx]]
             h7_casos = h7_casos.groupby(['CountryName','fecha']).mean().reset_index()
             h7_casos['pred'] = h7_casos['pred'].rolling(window=7, min_periods=1).mean()
             h7_casos['pred_sir'] = h7_casos['pred_sir'].rolling(window=7, min_periods=1).mean()
@@ -127,7 +124,7 @@ try:
             none_waning = none_waning[none_waning['CountryName'] == country2]
             none_waning['fecha'] = pd.to_datetime(none_waning['fecha'])
             none_waning['month'] = none_waning['fecha'].dt.strftime('%Y-%m')
-            none_waning = none_waning[none_waning['month'] ==fecha_elegida]
+            none_waning = none_waning[none_waning['month'] == months_list_short[idx]]
             none_waning = none_waning.groupby(['CountryName','fecha']).mean().reset_index()
             none_waning['pred'] = none_waning['pred'].rolling(window=7, min_periods=1).mean()
             none_waning['pred_sir'] = none_waning['pred_sir'].rolling(window=7, min_periods=1).mean()
@@ -135,7 +132,7 @@ try:
             none_casos = none_casos[none_casos['CountryName'] == country2]
             none_casos['fecha'] = pd.to_datetime(none_casos['fecha'])
             none_casos['month'] = none_casos['fecha'].dt.strftime('%Y-%m')
-            none_casos = none_casos[none_casos['month'] == fecha_elegida]
+            none_casos = none_casos[none_casos['month'] == months_list_short[idx]]
             none_casos = none_casos.groupby(['CountryName','fecha']).mean().reset_index()
             none_casos['pred'] = none_casos['pred'].rolling(window=7, min_periods=1).mean()
             none_casos['pred_sir'] = none_casos['pred_sir'].rolling(window=7, min_periods=1).mean()
@@ -143,7 +140,7 @@ try:
             xprize = xprize[xprize['CountryName'] == country2]
             xprize['fecha'] = pd.to_datetime(xprize['fecha'])
             xprize['month'] = xprize['fecha'].dt.strftime('%Y-%m')
-            xprize = xprize[xprize['month'] == fecha_elegida]
+            xprize = xprize[xprize['month'] == months_list_short[idx]]
             xprize = xprize.groupby(['CountryName','fecha']).mean().reset_index()
             xprize['pred'] = xprize['pred'].rolling(window=7, min_periods=1).mean()
             xprize['pred_sir'] = xprize['pred_sir'].rolling(window=7, min_periods=1).mean()
@@ -156,40 +153,23 @@ try:
             fig.add_trace(go.Scatter(x = xprize['fecha'], y = xprize['pred'], name = "XPrize", line = dict(color = 'black', width = 2)))
             fig.add_trace(go.Scatter(x = h7_waning['fecha'], y = h7_waning['truth'], name = "Ground Truth Daily New Cases", line = dict(color = 'orange', width = 4, dash = 'dash')))
         else: 
-            h7_waning_europe['fecha'] = pd.to_datetime(h7_waning_europe['fecha'], format = '%Y-%m-%d')
+            h7_waning_europe['fecha'] = pd.to_datetime(h7_waning_europe['fecha'])
             h7_waning_europe['month'] = h7_waning_europe['fecha'].dt.strftime('%Y-%m')
-            fecha_elegida = pd.to_datetime(months_list_short[idx]).strftime('%Y-%m')
-            h7_waning_europe['mes_pertenencia'] = h7_waning_europe['fecha'].dt.strftime('%m')
-            # Lo pasamos a entero
-            h7_waning_europe['mes_pertenencia'] = h7_waning_europe['mes_pertenencia'].astype(int)
-            # Ahora usamos el to_numeric para ordenar los meses
-            h7_waning_europe['mes_pertenencia'] = pd.to_numeric(h7_waning_europe['mes_pertenencia'])
-            # Nos quedamos con las filas del mes elegido
-            h7_waning_europe = h7_waning_europe[h7_waning_europe['month'] == fecha_elegida]
-            h7_waning_europe = h7_waning_europe.groupby(['mes_pertenencia']).mean().reset_index()
+            h7_waning_europe = h7_waning_europe[h7_waning_europe['month'] == months_list_short[idx]]
+            h7_waning_europe = h7_waning_europe.groupby(['fecha']).mean().reset_index()
             h7_waning_europe['pred_h7_waning'] = h7_waning_europe['pred_h7_waning'].rolling(window=7, min_periods=1).mean()
             h7_waning_europe['truth'] = h7_waning_europe['truth'].rolling(window=7, min_periods=1).mean()
             
-            h7_waning_none_europe['fecha'] = pd.to_datetime(h7_waning_none_europe['fecha'], format = '%Y-%m-%d')
+            h7_waning_none_europe['fecha'] = pd.to_datetime(h7_waning_none_europe['fecha'])
             h7_waning_none_europe['month'] = h7_waning_none_europe['fecha'].dt.strftime('%Y-%m')
-            h7_waning_none_europe['mes_pertenencia'] = h7_waning_none_europe['fecha'].dt.strftime('%m')
-            # Lo pasamos a entero
-            h7_waning_none_europe['mes_pertenencia'] = h7_waning_none_europe['mes_pertenencia'].astype(int)
-            # Ahora usamos el to_numeric para ordenar los meses
-            h7_waning_none_europe['mes_pertenencia'] = pd.to_numeric(h7_waning_none_europe['mes_pertenencia'])
-            h7_waning_none_europe = h7_waning_none_europe[h7_waning_none_europe['month'] ==fecha_elegida]
-            h7_waning_none_europe = h7_waning_none_europe.groupby(['mes_pertenencia']).mean().reset_index()
+            h7_waning_none_europe = h7_waning_none_europe[h7_waning_none_europe['month'] == months_list_short[idx]]
+            h7_waning_none_europe = h7_waning_none_europe.groupby(['fecha']).mean().reset_index()
             h7_waning_none_europe['pred_h7_waning'] = h7_waning_none_europe['pred_h7_waning'].rolling(window=7, min_periods=1).mean()
 
-            h7_waning_xprize_europe['fecha'] = pd.to_datetime(h7_waning_xprize_europe['fecha'], format = '%Y-%m-%d')
+            h7_waning_xprize_europe['fecha'] = pd.to_datetime(h7_waning_xprize_europe['fecha'])
             h7_waning_xprize_europe['month'] = h7_waning_xprize_europe['fecha'].dt.strftime('%Y-%m')
-            h7_waning_xprize_europe['mes_pertenencia'] = h7_waning_xprize_europe['fecha'].dt.strftime('%m')
-            # Lo pasamos a entero
-            h7_waning_xprize_europe['mes_pertenencia'] = h7_waning_xprize_europe['mes_pertenencia'].astype(int)
-            # Ahora usamos el to_numeric para ordenar los meses
-            h7_waning_xprize_europe['mes_pertenencia'] = pd.to_numeric(h7_waning_xprize_europe['mes_pertenencia'])
-            h7_waning_xprize_europe = h7_waning_xprize_europe[h7_waning_xprize_europe['month'] == fecha_elegida]
-            h7_waning_xprize_europe = h7_waning_xprize_europe.groupby(['mes_pertenencia']).mean().reset_index()
+            h7_waning_xprize_europe = h7_waning_xprize_europe[h7_waning_xprize_europe['month'] == months_list_short[idx]]
+            h7_waning_xprize_europe = h7_waning_xprize_europe.groupby(['fecha']).mean().reset_index()
             h7_waning_xprize_europe['pred_h7_waning'] = h7_waning_xprize_europe['pred_h7_waning'].rolling(window=7, min_periods=1).mean()
             
             fig = go.Figure()
